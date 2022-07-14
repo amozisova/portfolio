d4d8087fd9b04654d1677d79039e5a8a4329b157
amozisova <amozisova@gmail.com>
2022-07-14 16:25:46 +0200

    updated

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+include_once 'language.php';
+?>
+
+<!DOCTYPE html>
+<html lang="cs-cz">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <div class="container" id="home">
+        <nav class="navbar">
+
+            <div class="nav-container">
+            <span class="nav-item">
+                    <a class="nav-link" href="#home"><img src="img/home_wh.jpg" alt="home icon"></a>
+                </span>
+                <span class="nav-item">
+                    <a class="nav-link" href="#aboutme">o mně</a>
+                </span>
+                <span class="nav-item">
+                    <a class="nav-link" href="#skills">dovednosti</a>
+                </span>
+
+                <span class="nav-item">
+                    <a class="nav-link" href="#projects">projekty</a>
+                </span>
+
+                <span class="nav-item">
+                    <a class="nav-link" href="#contact">kontakt</a>
+                </span>
+            </div>
+        </nav>
+
+
+        <section class=content>
+            <div class="title">
+                <h1>Dobrý den.
+                    <br>Jsem <span class="name">Alžběta Možíšová</span>.
+                </h1>
+            </div>
+
+            <div class="subtitle">
+                <div class="arrow">
+                    <a href="#aboutme"><img src="img/arrow_down.png" alt="arrow down"></a>
+                </div>
+                <div class="subheading">
+                    <h3>Programátorka, která čeká na svou příležitost.</h3>
+                    <div class="contactIcons">
+                        <img src="img/message.png" alt="message icon">
+                        <img src="img/github.png" alt="GitHub icon">
+                        <img src="img/linkedin-logo.png" alt="LinkedIn icon">
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+
+
+    </div>
+
+
+    <div class="container">
+        <section id="aboutme">
+            <div class="section_title">
+                <h1>O mně</h1>
+            </div>
+            <div class="aboutme_content">
+                <div>
+                    <img src="img/mozisova_face.jpg" alt="Alzbeta Mozisova" height="223" width="200" class="story_img">
+                </div>
+
+                <div class="storybox">
+                    <div class="story_text">
+                        Už během studia na střední škole mě zajímaly počítače, koupila jsem si učebnici HTML a učila se psát web v poznámkovém bloku… Moje první webová stránka byl deníček, kam jsem zapisovala své zážitky. Další stránka byla o hudbě, která mě zajímala. Během let jsem občas vytvořila webové stránky pro někoho známého, pro nový studijní obor, pro studentský spolek, který jsme založili…
+
+                        <p>Ale ve svém studiu jsem se vydala humanitním směrem a věnovala se sociologii. Řadu let jsem se profesně pohybovala v sociální oblasti, převážně v neziskovém sektoru, v tématech domácího násilí a ohrožených dětí.</p>
+
+                        <p>Ve svých 35 letech jsem se však rozhodla pro zásadní kariérní změnu – vzpomněla jsem si na svou zálibu ve vytváření webů, na svůj zájem o počítače a internet, probudila se ve mně touha zase něco vytvářet (od nul a jedniček) a vydala jsem se směrem k IT, s cílem nezůstat u webových stránek, ale naučit se programovat.</p>
+                        <p>Teď čekám na svou první pracovní příležitost, která mi umožní být skutečně programátorkou a věnovat se tomu naplno.</p>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+
+
+
+
+    <script src=""></script>
+</body>
+
+</html>
