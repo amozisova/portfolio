ecafc5de14b7d60afad51e2ea4dad5e7a4456bfb
Alzbeta Mozisova <97987968+amozisova@users.noreply.github.com>
2022-07-15 10:00:42 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,56 +10,92 @@ include_once 'language.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
+    <div class="container" id="home">
+        <nav class="navbar">
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" aria-current="page" href="#">Home</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                <ul class="navbar-nav navbar-right me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#aboutme">O mně</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#skills">Dovednosti</a>
-                    </li>
+            <div class="nav-container">
+            <span class="nav-item">
+                    <a class="nav-link" href="#home"><img src="img/home.jpg" alt="home icon"></a>
+                </span>
+                <span class="nav-item">
+                    <a class="nav-link" href="#aboutme">o mně</a>
+                </span>
+                <span class="nav-item">
+                    <a class="nav-link" href="#skills">dovednosti</a>
+                </span>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#projects">Projekty</a>
-                    </li>
+                <span class="nav-item">
+                    <a class="nav-link" href="#projects">projekty</a>
+                </span>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#contact">Kontakt</a>
-                    </li>
-                </ul>
+                <span class="nav-item">
+                    <a class="nav-link" href="#contact">kontakt</a>
+                </span>
+            </div>
+        </nav>
+
+
+        <section class=content>
+            <div class="title">
+                <h1>Dobrý den.
+                    <br>Jsem <span class="name">Alžběta Možíšová</span>.
+                </h1>
+            </div>
+
+            <div class="subtitle">
+                <div class="arrow">
+                    <a href="#aboutme"><img src="img/arrow_down.png" alt="arrow down"></a>
+                </div>
+                <div class="subheading">
+                    <h3>Programátorka, která čeká na svou příležitost.</h3>
+                    <div class="contactIcons">
+                        <img src="img/message.png" alt="message icon">
+                        <img src="img/github.png" alt="GitHub icon">
+                        <img src="img/linkedin-logo.png" alt="LinkedIn icon">
+                    </div>
+                </div>
 
             </div>
-        </div>
-    </nav>
+        </section>
 
 
 
+    </div>
 
-
-
-
-    <header>
-
-    </header>
 
     <div class="container">
-        <div class="row">
-            <div class="col"></div>
-        </div>
+        <section id="aboutme">
+            <div class="section_title">
+                <h1>O mně</h1>
+            </div>
+            <div class="aboutme_content">
+                <div>
+                    <img src="img/mozisova_face.jpg" alt="Alzbeta Mozisova" height="223" width="200" class="story_img">
+                </div>
+
+                <div class="storybox">
+                    <div class="story_text">
+                        Už během studia na střední škole mě zajímaly počítače, koupila jsem si učebnici HTML a učila se psát web v poznámkovém bloku… Moje první webová stránka byl deníček, kam jsem zapisovala své zážitky. Další stránka byla o hudbě, která mě zajímala. Během let jsem občas vytvořila webové stránky pro někoho známého, pro nový studijní obor, pro studentský spolek, který jsme založili…
+
+                        <p>Ale ve svém studiu jsem se vydala humanitním směrem a věnovala se sociologii. Řadu let jsem se profesně pohybovala v sociální oblasti, převážně v neziskovém sektoru, v tématech domácího násilí a ohrožených dětí.</p>
+
+                        <p>Ve svých 35 letech jsem se však rozhodla pro zásadní kariérní změnu – vzpomněla jsem si na svou zálibu ve vytváření webů, na svůj zájem o počítače a internet, probudila se ve mně touha zase něco vytvářet (od nul a jedniček) a vydala jsem se směrem k IT, s cílem nezůstat u webových stránek, ale naučit se programovat.</p>
+                        <p>Teď čekám na svou první pracovní příležitost, která mi umožní být skutečně programátorkou a věnovat se tomu naplno.</p>
+                    </div>
+                </div>
+            </div>
+
     </div>
+
+
+
 
     <script src=""></script>
 </body>
